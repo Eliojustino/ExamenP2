ea541318d0a5eb2509feb36f2801c7ba64aecd4e
Eliojustino <126814395+Eliojustino@users.noreply.github.com>
2025-05-09 17:55:04 -0600

    Update test_fibonacci.py

--- examenP2/test_fibonacci.py
+++ examenP2/test_fibonacci.py
@@ -5,7 +5,7 @@ def test_fib onacci () :
 	assert fibonacci (0) == 0
 	assert fibonacci (1) == 1
 	assert fibonacci (5) == 5
-	print ( " Pruebas pasadas ! " )
+	print ( " Pruebas pasadas ! Elio 806 " )
 
 if __name__ == " __main__ " :
 	test _fibonac ci ()
