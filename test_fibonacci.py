76b29555fe0d8c21aab24529b661c6d2d69bda04
Eliojustino <126814395+Eliojustino@users.noreply.github.com>
2025-05-09 18:00:59 -0600

    Update test_fibonacci.py

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,7 +1,7 @@
 import timeit
 from fibonacci import fibonacci
 
-def test_ fibonacci () :
+def test_fibonacci () :
 	assert fibonacci (0) == 0
 	assert fibonacci (1) == 1
 	assert fibonacci (5) == 5
